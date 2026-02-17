3dc7e4f8355aceaf0111558514487baf9d81e244
Duygu Aslan <duygu.aslan@sestek.com>
2026-02-17 15:21:58 +0300

    checkpoint: angular basics + routing attempt

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {Child} from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[FormsModule, Child,RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-app');
+  title= 'angular öğreniyorum';
+
+  counter=0;
+
+  increase(){
+    this.counter++;
+  }
+
+  isVisible=  false;
+
+  toggle() {
+    this.isVisible = !this.isVisible;
+
+  }
+
+  items= ["angular", "react","vue"];
+
+  name = '';
+
+  user= {
+    name : '',
+    email: '' };
+  
+    onSubmit () {
+
+      console.log("Form Gönderildi:", this.user);
+    }
+
+    parentMessage = "Merhaba Child";
+
 }
